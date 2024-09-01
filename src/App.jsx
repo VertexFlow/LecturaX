617ec9fbd3acfc2c3d099d00fc8725e449277a0e
Harshil Gupta <harshilgupta.2005@gmail.com>
2024-09-01 23:41:32 +0530

    Add: Profile component

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,47 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Signin from "./pages/signin";
 import Signup from "./pages/signup";
-// import Home from "./pages/home";
+import Home from "./pages/home";
 import NoPage from "./pages/nopage";
 import Layout from "./components/layout";
 import Dashboard from "./pages/dashboard";
+import Profile from "./pages/profile";
 
 function App() {
+	const studentProfileData = {
+		name: "John Doe",
+		email: "john.doe@example.com",
+		class: "10th Grade",
+	};
+
+	const teacherProfileData = {
+		name: "Jane Smith",
+		email: "jane.smith@example.com",
+	};
 	return (
 		<>
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Navbar />}>
-						{/* <Route index element={<Home />} /> */}
+						<Route index element={<Home />} />
 						<Route path="/signin" element={<Signin />} />
 						<Route path="/signup" element={<Signup />} />
 					</Route>
 					<Route path="/dashboard" element={<Layout />}>
 						<Route index element={<Dashboard />} />
+						<Route
+							path="profile"
+							element={
+								<Profile userType="student" profileData={studentProfileData} />
+							}
+						/>
 					</Route>
 					<Route path="*" element={<NoPage />} />
 				</Routes>
 			</BrowserRouter>
+
+			{/* <Profile userType="student" profileData={studentProfileData} />
+			<Profile userType="teacher" profileData={teacherProfileData} /> */}
 		</>
 	);
 }
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -4,9 +4,14 @@ import "../styles/layout.css";
 
 const Layout = ({ username = "Username" }) => {
 	const [sidebarOpen, setSidebarOpen] = useState(true);
+	const [dropdown, setDropdown] = useState(false);
 
 	const toggleSidebar = () => {
 		setSidebarOpen(!sidebarOpen);
+	};
+
+	const toggleDropdown = () => {
+		setDropdown(!dropdown);
 	};
 
 	return (
@@ -18,19 +23,27 @@ const Layout = ({ username = "Username" }) => {
 						<span></span>
 						<span></span>
 					</div>
-					<h1>
-						<Link to="/" className="logo">
-							LecturaX
-						</Link>
-					</h1>
+					<Link to="/" className="logo">
+						LecturaX
+					</Link>
 				</div>
 				<div id="end">
-					<div className="profile-dropdown">
+					<img src="/assets/notification.svg" alt="Bell Icon" />
+					<div className="profile-dropdown" onClick={toggleDropdown}>
 						<span>{username}</span>
-						<div className="profile-dropdown-content">
-							<a href="#">Profile</a>
-							<a href="#">Settings</a>
-							<a href="#">Logout</a>
+						<div className={`profile-menu ${dropdown ? "expand" : ""}`}>
+							<Link to="/dashboard/profile" className="profile-option">
+								View Profile
+							</Link>
+							<Link to="/" className="profile-option">
+								Edit Profile
+							</Link>
+							<Link to="/" className="profile-option">
+								Change Password
+							</Link>
+							<Link to="/" className="profile-option">
+								Logout
+							</Link>
 						</div>
 					</div>
 				</div>
