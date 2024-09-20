e91c2e0465af2bd6945e8a9ec5d2d4e97a21f0f7
Harshil Gupta <144909381+hars-21@users.noreply.github.com>
2024-09-20 23:22:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:3000/",
+      "/api": "https://lecturax-api.onrender.com",
     },
   },
 });
